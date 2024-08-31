399f2dd2db87707aee7d68dbe637436648351545
Vlad Osipov <vladyslav.osip@gmail.com>
2024-08-31 13:48:08 +0300

    add style and gallety

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-function getElementWidth(content, padding, border) {
-    let contents = parseFloat(content);
-    let paddings = parseFloat(padding);
-    let borders = parseFloat(border);
-    return contents + paddings * 2 + borders * 2;
-};
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
